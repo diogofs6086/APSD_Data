150c0350b4ef328e5f79e18acbb102516dd02837
Diogo F. S <66284866+diogofs6086@users.noreply.github.com>
2020-08-20 22:36:45 -0300

    Update average_dataset_and_figure_SCRIPT.R

--- APSD_of_286.8_ppm_of_natural_boron/average_dataset_and_figure_SCRIPT.R
+++ APSD_of_286.8_ppm_of_natural_boron/average_dataset_and_figure_SCRIPT.R
@@ -3,8 +3,7 @@
 # and its uncertainty. Also, the figure of the average APSD
 # can be created as the last step of the script.
 
-# setwd("C:/Users/scape/Dropbox/Doutorado/Final/Paper da tese/arquivos para a ANE/Data_in_brief/dados/15kg")
-# setwd("/media/scape/98A8F45FA8F43D76/Users/scape/Dropbox/Doutorado/Final/Paper da tese/arquivos para a ANE/Data_in_brief/dados/15kg")
+# setwd("~/APSD_of_286.8_ppm_of_natural_boron")
 
 # Needed packages
 library(dplyr)
